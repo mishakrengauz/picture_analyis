7ea1c5978c77fed344868fcabf29c63294231a4e
Михаил Кренгауз <mihailkrengauz@MacBook-Pro-Mihail-Krengauz.local>
2019-02-22 15:01:03 +0300

    Добавил парсер аргументов командной строки.

--- something.py
+++ something.py
@@ -1,7 +1,9 @@
 import argparse
 from PIL import Image, ImageDraw
 
-brightness = int(input('brightness:'))
+parser = argparse.ArgumentParser(description='Image processing.')
+parser.add_argument('-b','--brightness', help='Minimal brightness', required = True)
+args = parser.parse_args()
 image = Image.open("test.jpg")
 draw = ImageDraw.Draw(image)
 width = image.size[0]
@@ -14,6 +16,6 @@ for i in range(width):
         b = pix[i, j][1]
         c = pix[i, j][2]
         S = (a*0.3 + b*0.59 + c*0.11)
-        if S > brightness:
+        if S > int(args.brightness):
             print(S)
 print(width, height)
